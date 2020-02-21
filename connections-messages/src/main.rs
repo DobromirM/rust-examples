d092b675b3a9ff09a772e549028a3e5509b98ede
DobromirM <mr.d.marinov@gmail.com>
2020-02-21 17:49:35 +0000

    * Changed the connection message example to add a runtime.

--- connections-messages/src/main.rs
+++ connections-messages/src/main.rs
@@ -6,24 +6,30 @@ use tokio::net::TcpStream;
 use std::{thread, time};
 use tokio::sync::mpsc;
 use url;
+use std::future::Future;
 
-struct Connection {}
+struct Connection {
+    url: url::Url,
+    rx: mpsc::Receiver<Message>,
+}
 
 impl Connection {
-    async fn new(host: &str) -> mpsc::Sender<Message> {
+    fn new(host: &str) -> (Connection, mpsc::Sender<Message>) {
         let url = url::Url::parse(&host).unwrap();
-        let (ws_stream, _) = connect_async(&url).await.expect("Failed to connect");
-
-        let (write_stream, read_stream) = ws_stream.split();
         let (tx, rx) = mpsc::channel(5);
 
+        return (Connection { url, rx }, tx);
+    }
+
+    async fn open(self) {
+        let (ws_stream, _) = tokio::spawn(connect_async(self.url)).await.unwrap().unwrap();
+        let (write_stream, read_stream) = ws_stream.split();
+
         let receive = Connection::receive_messages(read_stream);
-        let send = rx.map(Ok).forward(write_stream);
+        let send = self.rx.map(Ok).forward(write_stream);
 
         tokio::spawn(send);
         tokio::spawn(receive);
-
-        return tx;
     }
 
     async fn receive_messages(mut read_stream: SplitStream<WebSocketStream<TcpStream>>) {
@@ -39,15 +45,33 @@ impl Connection {
     }
 }
 
-#[tokio::main]
-async fn main() {
-    let tx = Connection::new("ws://127.0.0.1:9001").await;
+struct Runtime {
+    rt: tokio::runtime::Runtime,
+}
 
+impl Runtime {
+    fn new() -> Runtime {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        return Runtime { rt };
+    }
+
+    fn schedule_task<F>(&self, task: impl Future<Output=F> + std::marker::Send + 'static)
+        where F: std::marker::Send + 'static {
+        &self.rt.spawn(async move { task.await });
+    }
+}
+
+
+fn main() {
+    let runtime = Runtime::new();
+    let (connection, tx) = Connection::new("ws://127.0.0.1:9001");
+
+    runtime.schedule_task(connection.open());
     let tx_clone = mpsc::Sender::clone(&tx);
-    tokio::spawn(async move { Connection::send_message(tx_clone, String::from(r#"@sync(node:"/unit/foo",lane:info)"#)).await });
+    runtime.schedule_task(Connection::send_message(tx_clone, String::from(r#"@sync(node:"/unit/foo",lane:info)"#)));
     thread::sleep(time::Duration::from_secs(2));
 
     let tx_clone = mpsc::Sender::clone(&tx);
-    tokio::spawn(async move { Connection::send_message(tx_clone, String::from(r#"@sync(node:"/unit/foo",lane:info)"#)).await });
+    runtime.schedule_task(Connection::send_message(tx_clone, String::from(r#"@sync(node:"/unit/foo",lane:info)"#)));
     thread::sleep(time::Duration::from_secs(10))
 }
